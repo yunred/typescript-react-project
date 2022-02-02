aa74f4601983fe5e55e4acc29932402723bdc1fd
SEOYUN KIM <nado0903@gmail.com>
2022-02-02 23:18:30 +0900

    Add: 초기 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
 import './App.css';
+import Main from 'Pages/Main';
+import Category from 'Pages/Category';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Route path="/" component={Main} />
+      <Route path="/category/:selected" component={Category} />
+    </>
   );
 }
 
