e22b29c7ac68ea2636b4ccd1fe3c89532731845b
SEOYUN KIM <nado0903@gmail.com>
2022-02-05 17:55:09 +0900

    Add : 카테고리 링크 추가

--- src/Pages/Main/index.tsx
+++ src/Pages/Main/index.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
 import PostInsert from 'components/PostInsert';
 import PostList from 'components/PostList';
+import CategorySelect from 'components/CategorySelect';
 
 const Main: FC = () => {
   return (
     <>
       <>
+        <CategorySelect />
         <PostInsert />
         <PostList />
       </>
---
+++ src/components/CategorySelect.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const CategorySelect = () => {
+  return (
+    <>
+      <li>
+        <Link to="/category/전체게시판">전체게시판</Link>
+      </li>
+      <li>
+        <Link to="/category/게시판1">게시판1</Link>
+      </li>
+      <li>
+        <Link to="/category/게시판2">게시판2</Link>
+      </li>
+    </>
+  );
+};
+
+export default CategorySelect;
