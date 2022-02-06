511064f51fe9facd3009bf244ed3824c6c13f012
SEOYUN KIM <nado0903@gmail.com>
2022-02-06 22:51:55 +0900

    Add: CategorySelect 스타일 추가

--- src/components/CategorySelect.tsx
+++ src/components/CategorySelect.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const CategorySelect = () => {
   return (
-    <>
-      <li>
+    <CategorySelectUL>
+      <CategoryLI>
         <Link to="/category/전체게시판">전체게시판</Link>
-      </li>
-      <li>
+      </CategoryLI>
+      <CategoryLI>
         <Link to="/category/게시판1">게시판1</Link>
-      </li>
-      <li>
+      </CategoryLI>
+      <CategoryLI>
         <Link to="/category/게시판2">게시판2</Link>
-      </li>
-      <li>
+      </CategoryLI>
+      <CategoryLI>
         <Link to="/bookmark">저장한 게시물</Link>
-      </li>
-    </>
+      </CategoryLI>
+    </CategorySelectUL>
   );
 };
+const CategorySelectUL = styled.ul`
+  width: 80%;
+  margin: 0 auto;
+  display: flex;
+  text-align: center;
+  list-style: none;
+  margin-bottom: 10px;
+`;
 
+const CategoryLI = styled.li`
+  flex: 1;
+  height: 40px;
+  line-height: 40px; //세로 중앙정렬
+  background-color: #ffffff;
+  a {
+    text-decoration: none;
+    display: block; //a 태그 범위
+    color: black;
+    :hover {
+      opacity: 0.7;
+      background-color: #fcfcfc;
+    }
+  }
+`;
 export default CategorySelect;
